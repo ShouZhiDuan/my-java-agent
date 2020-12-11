9d64716b3598eac20d85ede300ceaae0ae1022b9
段守志 <shouzhi.duan@nvxclouds.com>
2020-12-11 14:47:19 +0800

    init project

---
+++ src/main/java/com/java/agent/JavaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.java.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAgentApplication.class, args);
+    }
+
+}
